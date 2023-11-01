75836aba705d740ae62eb0ad65e4b9a513413820
bazileros <zalisilesithule@gmail.com>
2023-11-01 23:06:36 +0200

    added 3 macros

--- binary_trees.h
+++ binary_trees.h
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stddef.h>
+#include <math.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <unistd.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -76,8 +80,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 /*Checks if two trees are identical*/
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-		const binary_tree_t *second);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
