b883e3154b90e3aa575e695e74c9c9b55d11d57a
bazileros <zalisilesithule@gmail.com>
2023-11-02 01:30:08 +0200

    changed logic

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -21,16 +21,21 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	new_node = binary_tree_node(parent, value);
+	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
-		return (NULL);
-
-	if (parent->right != NULL)
 	{
-		parent->right = new_node;
-		parent->right->right = parent->right;
+		free(new_node);
+		return (NULL);
 	}
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = parent->right;
 	parent->right = new_node;
+
+	if (new_node->right)
+	new_node->right->parent = new_node;
 
 	return (new_node);
 }
