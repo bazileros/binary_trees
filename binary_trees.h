d90ae0248c4ba0580516261e6a96f4023ea0ab7c
bazileros <zalisilesithule@gmail.com>
2023-11-01 22:44:51 +0200

    prototypes && struct defs

---
+++ binary_trees.h
@@ -0,0 +1,102 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+/**
+ * struct levelorder_queue_s - Level order traversal queue.
+ * @node: A node of a binary tree.
+ * @next: The next node to traverse to in the binary tree.
+ */
+typedef struct levelorder_queue_s
+{
+	binary_tree_t *node;
+	struct levelorder_queue_s *next;
+} levelorder_queue_t;
+
+/* Create a node tree struct */
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
+/*Creates a binary tree node*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+/*Inserts a left child in a binary tree*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+/*Inserts a right child in a binary tree*/
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+/*Deletes an entire binary tree*/
+void binary_tree_delete(binary_tree_t *tree);
+/*Checks if a node is a leaf*/
+int binary_tree_is_leaf(const binary_tree_t *node);
+/*Checks if a node is the root*/
+int binary_tree_is_root(const binary_tree_t *node);
+/*Checks if a binary tree is empty*/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+/*Checks if a binary tree is full*/
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+/*Checks if a binary tree is full*/
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_height(const binary_tree_t *tree);
+/*Checks if two trees are identical*/
+size_t binary_tree_depth(const binary_tree_t *tree);
+/*Checks if two trees are identical*/
+size_t binary_tree_size(const binary_tree_t *tree);
+/*Checks if two trees are identical*/
+size_t binary_tree_leaves(const binary_tree_t *tree);
+/*Checks if two trees are identical*/
+size_t binary_tree_nodes(const binary_tree_t *tree);
+/*Checks if two trees are identical*/
+int binary_tree_balance(const binary_tree_t *tree);
+/*Checks if two trees are identical*/
+int binary_tree_is_full(const binary_tree_t *tree);
+/*Checks if two trees are identical*/
+int binary_tree_is_perfect(const binary_tree_t *tree);
+/*Checks if two trees are identical*/
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+/*Checks if two trees are identical*/
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
+bst_t *array_to_bst(int *array, size_t size);
+bst_t *bst_search(const bst_t *tree, int value);
+bst_t *bst_remove(bst_t *root, int value);
+int binary_tree_is_avl(const binary_tree_t *tree);
+avl_t *avl_insert(avl_t **tree, int value);
+avl_t *array_to_avl(int *array, size_t size);
+avl_t *avl_remove(avl_t *root, int value);
+avl_t *sorted_array_to_avl(int *array, size_t size);
+int binary_tree_is_heap(const binary_tree_t *tree);
+heap_t *heap_insert(heap_t **root, int value);
+heap_t *array_to_heap(int *array, size_t size);
+int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
+void binary_tree_print(const binary_tree_t *tree);
+
+#endif
