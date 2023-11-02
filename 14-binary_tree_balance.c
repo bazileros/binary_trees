7340b3a4c9574baf1024088df2b9b92eb608cb12
bazileros <zalisilesithule@gmail.com>
2023-11-03 00:57:29 +0200

    fixed bug

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -18,7 +18,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_height - Measures the height of a binary tree.\
+ * binary_tree_height - Measures the height of a binary tree.
  *
  * @tree: A pointer to the root node of the tree to measure the height.
  *
